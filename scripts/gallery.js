e5a226a44993cb64f1cb0c9fcd8b6c2329bfea60
Jude Woolls <jwoolls01@gmail.com>
2025-07-29 17:09:16 +0100

    add the gallery button and image functionaliy with placeholder images

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,1 +1,30 @@
 const gallery = document.querySelector('.gallery');
+const galleryLeftButton = document.querySelector('.gallery-button:first-child');
+const galleryRightButton = document.querySelector('.gallery-button:last-child');
+
+let currentIndex = 0;
+const images = [
+    'images/Copilot_20250728_185951.png',
+    'images/pexels-olly-3806288.jpg',
+    'images/young-auto-mechanic-1024x683.jpg',
+]
+
+function updateGallery() {
+    gallery.style.backgroundImage = `url(${images[currentIndex]})`;
+    gallery.style.backgroundSize = 'cover';
+    gallery.style.backgroundPosition = 'center';
+    gallery.style.backgroundRepeat = 'no-repeat';
+    gallery.style.transition = 'background-image 0.5s ease-in-out';
+}
+
+galleryLeftButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateGallery();
+    console.log(`Current index: ${currentIndex}, Image: ${images[currentIndex]}`);
+});
+
+galleryRightButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateGallery();
+    console.log(`Current index: ${currentIndex}, Image: ${images[currentIndex]}`);
+});
